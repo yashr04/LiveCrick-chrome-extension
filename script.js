f4ce4c7cc69b81fd2933fcc29fd181de7c18ee2c
yashr04 <yashraj3247@gmail.com>
2024-11-14 17:14:45 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,25 @@
+async function getMatchData() {
+
+    return await fetch("https://api.cricapi.com/v1/currentMatches?apikey=88676767-cc2e-47b6-bf02-0f3cf9e4b70c&offset=0")
+        .then(data => data.json())
+        .then(data => {
+            if (data.status != "success")return;
+
+            const matchesList = data.data;
+
+            if(!matchesList)return [];
+            
+            //add your api key from cricketdata.org
+            const relevantData = matchesList.map(match => `${match.name}, ${match.status}`);
+
+            console.log({relevantData});
+
+            document.getElementById("matches").innerHTML = relevantData.map(match => `<li>${match} </li>`).join('');
+
+            return relevantData;
+
+        })
+        .catch(e => console.log(e));
+}
+
+getMatchData();
